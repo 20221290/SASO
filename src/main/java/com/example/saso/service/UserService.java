95d318045aedb6184a88ebee3a62038d6b5c1752
20221290 <1781720396@qq.com>
2024-07-23 20:33:54 +0800

    完成了用户的注册功能，以及更新及删除

---
+++ src/main/java/com/example/saso/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.saso.service;
+
+
+import com.example.saso.entity.User;
+
+
+public interface UserService{
+
+    public String register(User user);
+
+    String updateByUsername(User user);
+
+    String deleteByUsername(User user);
+}
---
+++ src/main/java/com/example/saso/service/impl/UserServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.saso.service.impl;
+
+
+import com.example.saso.entity.User;
+import com.example.saso.entity.UserExample;
+import com.example.saso.mapper.UserMapper;
+import com.example.saso.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public String register(User user) {
+        UserExample userExample =  new UserExample();
+        userExample.createCriteria().andUsernameEqualTo(user.getUsername());
+        List<User> list= userMapper.selectByExample(userExample);
+        //select * from user where username = user.getUsername();
+
+//        User userEquals = userMapper.findByUsername(user.getUsername());
+        if(list.isEmpty()){
+            if(user.getUsername().isEmpty()){
+                return "用户名不为空";
+            } else if (user.getPassword().isEmpty()) {
+                return "密码不为空";
+            }else {
+                userMapper.insert(user);
+                return "success";
+            }
+        }
+        return "注册失败，用户已存在";
+    }
+
+    /**
+     * @param
+     * @return
+     */
+    @Override
+    public String updateByUsername(User user) {
+        UserExample userExample =  new UserExample();
+        userExample.createCriteria().andUsernameEqualTo(user.getUsername());
+        List<User> list= userMapper.selectByExample(userExample);
+        if(!list.isEmpty()){
+            userMapper.updateByUsername(user);
+            return "success";
+        }
+        return "修改用户信息失败";
+    }
+
+    @Override
+    public String deleteByUsername(User user) {
+        UserExample userExample =  new UserExample();
+        userExample.createCriteria().andUsernameEqualTo(user.getUsername());
+        List<User> list= userMapper.selectByExample(userExample);
+        if(!list.isEmpty()){
+            userMapper.deleteByUsername(user);
+//            System.out.println(userMapper.deleteByUsername(user));
+            return "success";
+        }
+        return "销毁账号失败";
+    }
+}
